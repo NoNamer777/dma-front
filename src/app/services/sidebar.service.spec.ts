3318cb1b17298e345183b633d6003c277e2396c8
Oscar Wellner <oscar.wellner@gmail.com>
2021-06-17 21:45:55 +0200

    Add tests for SidebarService

--- src/app/services/sidebar.service.spec.ts
+++ src/app/services/sidebar.service.spec.ts
@@ -1,3 +1,43 @@
 import { SidebarService } from './sidebar.service';
+import { TestBed } from '@angular/core/testing';
 
-describe('SidebarService', () => {});
+describe('SidebarService', () => {
+
+  let sidebarService: SidebarService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ SidebarService ],
+    });
+
+    sidebarService = TestBed.inject(SidebarService);
+  })
+
+  it('should toggle opened state', () => {
+    expect(sidebarService.opened).toBe(false);
+
+    sidebarService.toggleSidebar();
+
+    expect(sidebarService.opened).toBe(true);
+  });
+
+  it('should coerce opened value to boolean', () => {
+    setState(null, false);
+    setState(undefined, false);
+    setState('', true);
+    setState(-1, true);
+    setState(0, true);
+    setState(1, true);
+    setState({ object: 'value' }, true);
+    setState({}, true);
+  });
+
+  function setState(value: any, expected: boolean): void {
+    sidebarService.opened = value;
+
+    expect(sidebarService.opened).toBe(expected);
+
+    // Reset the state to the default value.
+    sidebarService.opened = false;
+  }
+});
