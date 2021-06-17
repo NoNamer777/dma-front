ba9afe311afafe02801cdc89fff1c46a70c91939
Oscar Wellner <oscar.wellner@gmail.com>
2021-06-17 21:36:00 +0200

    Add tests for HeaderComponent

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,3 +1,52 @@
-import { HeaderComponent } from './header.component';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FontAwesomeTestingModule } from '@fortawesome/angular-fontawesome/testing';
 
-describe('HeaderComponent', () => {});
+import { HeaderComponent } from './header.component';
+import { SidebarService } from '../../services/sidebar.service';
+
+describe('HeaderComponent', () => {
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  let component: HeaderComponent;
+  let element: HTMLElement;
+
+  let sidebarService: SidebarService;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [ FontAwesomeTestingModule ],
+      declarations: [ HeaderComponent ],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+
+    sidebarService = TestBed.inject(SidebarService);
+
+    component = fixture.debugElement.componentInstance;
+    element = fixture.debugElement.nativeElement;
+
+    fixture.detectChanges();
+  });
+
+  it('should emit sidebar toggle on btn click',() => {
+    const toggleBtn = element.querySelector('#toggle-sidebar-btn') as HTMLButtonElement;
+    let btnIcon = toggleBtn.querySelector('fa-icon') as HTMLElement;
+
+    spyOn(component.toggleSidebar, 'emit');
+    spyOnProperty(sidebarService, 'opened', 'get').and.returnValue(true);
+
+    expect(component.toggleSidebar.emit).not.toHaveBeenCalled();
+    expect(btnIcon.getAttribute('icon')).toBe('bars');
+
+    toggleBtn.click();
+    fixture.detectChanges();
+
+    // Try to get the new icon after the toggle had happened.
+    btnIcon = toggleBtn.querySelector('fa-icon') as HTMLElement;
+
+    expect(component.toggleSidebar.emit).toHaveBeenCalled();
+    expect(btnIcon.getAttribute('icon')).toBe('times');
+  });
+});
