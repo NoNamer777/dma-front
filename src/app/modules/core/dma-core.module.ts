c0dc33f8f5330725119977980a33bae358abe05c
Oscar Wellner <oscar.wellner@gmail.com>
2021-10-16 17:12:42 +0200

    Move import location of the BrowserAnimationsModule

--- src/app/modules/core/dma-core.module.ts
+++ src/app/modules/core/dma-core.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DmaSharedModule } from '@dma-shared';
 import { DmaCenterComponent } from './components/dma-center/dma-center.component';
 import { DmaHeaderComponent } from './components/dma-header/dma-header.component';
 import { DmaSidebarComponent } from './components/dma-sidebar/dma-sidebar.component';
-import { DmaMaterialModule } from './dma-material.module';
 import { DmaRoutingModule } from './dma-routing.module';
 import { DmaHomeComponent } from './pages/dma-home/dma-home.component';
 import { DmaNotFoundComponent } from './pages/dma-not-found/dma-not-found.component';
 
 @NgModule({
-    imports: [BrowserModule, DmaMaterialModule, DmaRoutingModule, DmaSharedModule],
+    imports: [BrowserModule, BrowserAnimationsModule, DmaRoutingModule, DmaSharedModule],
     declarations: [DmaCenterComponent, DmaHeaderComponent, DmaSidebarComponent, DmaHomeComponent, DmaNotFoundComponent],
     exports: [DmaCenterComponent, DmaHeaderComponent, DmaHomeComponent, DmaNotFoundComponent],
 })
--- src/app/modules/shared/dma-material.module.ts
+++ src/app/modules/shared/dma-material.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDialogModule } from '@angular/material/dialog';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-    imports: [BrowserAnimationsModule, MatSidenavModule, MatDialogModule],
-    exports: [BrowserAnimationsModule, MatSidenavModule, MatDialogModule],
+    exports: [MatSidenavModule, MatDialogModule],
+    imports: [MatSidenavModule, MatDialogModule],
 })
 export class DmaMaterialModule {}
