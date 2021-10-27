6809583ad1e876886bc566d87d1aed2692ffcd5e
Oscar Wellner <oscar.wellner@gmail.com>
2021-10-27 23:59:38 +0200

    Change coverage rapport server port

--- serve-coverage.js
+++ serve-coverage.js
@@ -1,4 +1,4 @@
-const finalHandler = require('finalHandler');
+const finalhandler = require('finalhandler');
 const serveStatic = require('serve-static');
 const http = require('http');
 
@@ -7,10 +7,10 @@ const serve = serveStatic('coverage/dma-front', {
 });
 
 const server = http.createServer(function onRequest(request, response) {
-    serve(request, response, finalHandler(request, response));
+    serve(request, response, finalhandler(request, response));
 });
 
-const port = 4200;
+const port = 4300;
 const hostname = '192.168.178.94';
 
 server.listen(port, hostname, function onServerCreated() {
