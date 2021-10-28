2ba1a681618424a7c954d1cdff3703974748f08d
Oscar Wellner <oscar.wellner@gmail.com>
2021-10-29 00:22:00 +0200

    Create coverage rapport servers both on private IP and localhost

--- serve-coverage.js
+++ serve-coverage.js
@@ -6,13 +6,21 @@ const serve = serveStatic('coverage/dma-front', {
     index: ['index.html', 'index.htm'],
 });
 
-const server = http.createServer(function onRequest(request, response) {
+const remoteServer = http.createServer(function onRequest(request, response) {
+    serve(request, response, finalhandler(request, response));
+});
+
+const localServer = http.createServer(function onRequest(request, response) {
     serve(request, response, finalhandler(request, response));
 });
 
 const port = 4300;
 const hostname = '192.168.178.94';
 
-server.listen(port, hostname, function onServerCreated() {
-    console.log(`listening to http://${hostname}:${port}\n`);
+remoteServer.listen(port, hostname, function onServerCreated() {
+    console.log(`\nlistening to http://${hostname}:${port}`);
+});
+
+localServer.listen(port, 'localhost', function onServerCreated() {
+    console.log(`listening to http://localhost:${port}\n`);
 });
