04bc077bce93e6268ffcc0e1952f69825a6f9344
Oscar Wellner <oscar.wellner@gmail.com>
2021-10-29 00:16:47 +0200

    Add buttons to navigate between Spell pages

--- src/app/modules/spells-overview/pages/dma-spells-overview/dma-spells-overview.component.spec.ts
+++ src/app/modules/spells-overview/pages/dma-spells-overview/dma-spells-overview.component.spec.ts
@@ -2,16 +2,20 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogModule } from '@angular/material/dialog';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute, Route } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { environment } from '../../../../../environments/environment';
+import { dispatchEvent } from '../../../../../../testing/fake-events';
 import { DmaSpellsOverviewComponent } from './dma-spells-overview.component';
 import { DmaSpellCardComponent } from '@dma-spells-overview/components/dma-spell-card/dma-spell-card.component';
 import { Spell } from '@dma-shared/models';
 
 describe('DmaSpellsOverviewComponent', () => {
     let fixture: ComponentFixture<DmaSpellsOverviewComponent>;
-    let httpTestingController: HttpTestingController;
     let element: HTMLElement;
+
+    let httpTestingController: HttpTestingController;
 
     const mockSpell1 = new Spell('spell-1');
     mockSpell1.name = 'My Awesome Spell 1';
@@ -19,31 +23,150 @@ describe('DmaSpellsOverviewComponent', () => {
     const mockSpell2 = new Spell('spell-2');
     mockSpell2.name = 'My Awesome Spell 2';
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [HttpClientTestingModule, MatDialogModule, NoopAnimationsModule],
-            declarations: [DmaSpellsOverviewComponent, DmaSpellCardComponent],
-        }).compileComponents();
-    });
+    const testRoutes: Route[] = [
+        {
+            path: 'spells',
+            component: DmaSpellsOverviewComponent,
+        },
+    ];
 
-    beforeEach(() => {
+    function initialize(expectedResponse: Record<string, unknown>, pageNumber?: number): void {
+        let expectedUrl = `${environment.baseApiUrl}/spell`;
+
+        if (pageNumber !== undefined) {
+            expectedUrl += `?page=${pageNumber}`;
+
+            TestBed.overrideProvider(ActivatedRoute, {
+                useValue: {
+                    snapshot: {
+                        queryParams: {
+                            page: `${pageNumber}`,
+                        },
+                    },
+                },
+            });
+        }
         httpTestingController = TestBed.inject(HttpTestingController);
+
         fixture = TestBed.createComponent(DmaSpellsOverviewComponent);
 
         element = fixture.nativeElement;
 
         fixture.detectChanges();
 
-        httpTestingController.expectOne(`${environment.baseApiUrl}/spell`).flush({
-            content: [mockSpell1, mockSpell2],
-        });
+        httpTestingController.expectOne(expectedUrl).flush(expectedResponse);
 
         fixture.detectChanges();
+    }
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule,
+                MatDialogModule,
+                NoopAnimationsModule,
+                RouterTestingModule.withRoutes(testRoutes),
+            ],
+            declarations: [DmaSpellsOverviewComponent, DmaSpellCardComponent],
+        }).compileComponents();
     });
 
+    afterEach(() => httpTestingController.verify());
+
     it('should show received Spells as cards in the overview template', () => {
+        initialize({
+            content: [mockSpell1, mockSpell2],
+            first: true,
+            last: false,
+        });
+
         const spellCards = element.querySelectorAll('dma-spell-card');
 
         expect(spellCards.length).toBe(2);
+    });
+
+    it('should not be able to request the previous page on the first page', () => {
+        initialize({
+            content: [mockSpell1, mockSpell2],
+            first: true,
+            last: false,
+        });
+
+        expect(element.querySelector('button.previous-btn').getAttribute('disabled')).toBe('');
+    });
+
+    it('should not be able to request the next page on the last page', () => {
+        initialize({
+            content: [mockSpell1, mockSpell2],
+            first: false,
+            last: true,
+        });
+
+        expect(element.querySelector('button.next-btn').getAttribute('disabled')).toBe('');
+    });
+
+    it('should get the page number from the route', () => {
+        initialize(
+            {
+                content: [mockSpell1, mockSpell2],
+                first: false,
+                last: true,
+            },
+            2,
+        );
+
+        expect(element.querySelector('button.next-btn').getAttribute('disabled')).toBe('');
+        expect(element.querySelector('button.previous-btn').getAttribute('disabled')).toBe(null);
+    });
+
+    it('should be able to request the previous page when not on the first page', () => {
+        initialize(
+            {
+                content: [mockSpell1, mockSpell2],
+                first: false,
+                last: true,
+            },
+            1,
+        );
+
+        const previousBtn = element.querySelector('button.previous-btn');
+
+        expect(previousBtn.getAttribute('disabled')).toBe(null);
+
+        dispatchEvent(previousBtn, new MouseEvent('click'));
+        fixture.detectChanges();
+
+        httpTestingController.expectOne(`${environment.baseApiUrl}/spell`).flush({
+            content: [mockSpell1, mockSpell2],
+            first: true,
+            last: false,
+        });
+        fixture.detectChanges();
+
+        expect(previousBtn.getAttribute('disabled')).toBe('');
+    });
+
+    it('should be able to request the next page when not on the last page', () => {
+        initialize({
+            content: [mockSpell1, mockSpell2],
+            first: true,
+            last: false,
+        });
+
+        const nextBtn = element.querySelector('button.next-btn');
+
+        expect(nextBtn.getAttribute('disabled')).toBe(null);
+
+        dispatchEvent(nextBtn, new MouseEvent('click'));
+        fixture.detectChanges();
+
+        httpTestingController.expectOne(`${environment.baseApiUrl}/spell?page=1`).flush({
+            content: [mockSpell1, mockSpell2],
+            first: false,
+            last: true,
+        });
+        fixture.detectChanges();
+
+        expect(nextBtn.getAttribute('disabled')).toBe('');
     });
 });
--- src/app/modules/spells-overview/pages/dma-spells-overview/dma-spells-overview.component.ts
+++ src/app/modules/spells-overview/pages/dma-spells-overview/dma-spells-overview.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { DmaSpellsService } from '@dma-spells-overview';
 
 @Component({
@@ -8,16 +10,50 @@ import { DmaSpellsService } from '@dma-spells-overview';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DmaSpellsOverviewComponent implements OnInit {
+    /** Whether the page is currently waiting for a response. */
     waitingForResponse = false;
 
-    constructor(public spellsService: DmaSpellsService, private changeDetectorRef: ChangeDetectorRef) {}
+    constructor(
+        public spellsService: DmaSpellsService,
+        private changeDetectorRef: ChangeDetectorRef,
+        private router: Router,
+        private route: ActivatedRoute,
+    ) {}
 
     ngOnInit(): void {
+        this.getPageNumberFromRoute();
+        this.requestSpells();
+    }
+
+    /** Sends a GET request to get the Spells. */
+    requestSpells(): void {
         this.waitingForResponse = true;
 
         this.spellsService.getSpells().subscribe(() => {
             this.waitingForResponse = false;
             this.changeDetectorRef.markForCheck();
         });
+    }
+
+    /**
+     * Requests a specific page of Spells.
+     * @param pageNumber the page of Spells to get.
+     */
+    onRequestPage(pageNumber: number): void {
+        this.spellsService.pageNumber = pageNumber;
+
+        this.requestSpells();
+
+        this.router.navigate([], { queryParams: { page: this.spellsService.pageNumber } });
+    }
+
+    /**
+     * Gets the page number from the current route.
+     * @private
+     */
+    private getPageNumberFromRoute(): void {
+        if (this.route.snapshot.queryParams.page === undefined) return;
+
+        this.spellsService.pageNumber = parseInt(this.route.snapshot.queryParams.page, 10);
     }
 }
