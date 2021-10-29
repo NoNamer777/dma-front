787660b645de8049dc1e4a07130b2fd43236b258
Oscar Wellner <oscar.wellner@gmail.com>
2021-10-29 22:01:30 +0200

    Add test for requesting a specific Spells page

--- src/app/modules/spells-overview/services/spells/dma-spells.service.spec.ts
+++ src/app/modules/spells-overview/services/spells/dma-spells.service.spec.ts
@@ -1,9 +1,10 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { Spell } from '@dma-shared/models';
-import { DmaSpellsService } from './dma-spells.service';
+
 import { environment } from '../../../../../environments/environment';
+import { DmaSpellsService } from './dma-spells.service';
 import { Pageable } from '@dma-shared/models/pageable.model';
+import { Spell } from '@dma-shared/models';
 
 describe('DmaSpellsService', () => {
     let spellsService: DmaSpellsService;
@@ -13,10 +14,10 @@ describe('DmaSpellsService', () => {
     const spellMock = new Spell('spell-1');
     spellMock.name = 'My Awesome Spell';
     const spellPageMock = {
-        content: [
-            spellMock,
-        ],
+        content: [spellMock],
     } as Pageable<Spell>;
+
+    afterEach(() => httpTestingController.verify());
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -29,8 +30,8 @@ describe('DmaSpellsService', () => {
     });
 
     it('should return typed Spell objects', () => {
-        spellsService.getSpells().subscribe(() => {
-            expect(spellsService.spellsPage).toEqual(spellPageMock);
+        spellsService.getSpells().subscribe({
+            next: () => expect(spellsService.spellsPage).toEqual(spellPageMock),
         });
 
         httpTestingController.expectOne(`${environment.baseApiUrl}/spell`).flush({
@@ -40,6 +41,21 @@ describe('DmaSpellsService', () => {
                     name: 'My Awesome Spell',
                 },
             ],
+        });
+    });
+
+    it('should return a specific page of Spells', () => {
+        spellsService.pageNumber = 2;
+
+        spellsService.getSpells().subscribe({
+            next: (spellsPage) => expect(spellsPage.pageable.pageNumber).toBe(2),
+        });
+
+        httpTestingController.expectOne(`${environment.baseApiUrl}/spell?page=2`).flush({
+            content: [],
+            pageable: {
+                pageNumber: 2,
+            },
         });
     });
 });
--- src/app/modules/spells-overview/services/spells/dma-spells.service.ts
+++ src/app/modules/spells-overview/services/spells/dma-spells.service.ts
@@ -2,19 +2,33 @@ import { Injectable } from '@angular/core';
 import { DmaApiService } from '@dma-shared';
 import { Observable, tap } from 'rxjs';
 
-import { Spell } from '@dma-shared/models/entities';
 import { environment } from '../../../../../environments/environment';
 import { Pageable } from '@dma-shared/models/pageable.model';
+import { Spell } from '@dma-shared/models/entities';
 
 @Injectable({ providedIn: 'root' })
 export class DmaSpellsService {
+    /** The current page with Spells. */
     spellsPage: Pageable<Spell>;
+
+    /** The current page number. */
+    pageNumber = 0;
 
     constructor(private apiService: DmaApiService) {}
 
+    /**
+     * Sends a GET request to get a page of Spells.
+     * Optionally requests a specific page of Spells.
+     */
     getSpells(): Observable<Pageable<Spell>> {
+        if (this.pageNumber === 0) {
+            return this.apiService
+                .getPageableResource<Spell>(`${environment.baseApiUrl}/spell`, 'Spell')
+                .pipe(tap((spellsPage) => (this.spellsPage = spellsPage)));
+        }
+
         return this.apiService
-            .getPageableResource<Spell>(`${environment.baseApiUrl}/spell`, 'Spell')
+            .getPageableResource<Spell>(`${environment.baseApiUrl}/spell?page=${this.pageNumber}`, 'Spell')
             .pipe(tap((spellsPage) => (this.spellsPage = spellsPage)));
     }
 }
