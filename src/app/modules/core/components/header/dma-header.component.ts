5f684fbb16dffd22b30200c826269f23d998e060
Oscar Wellner <oscar.wellner@gmail.com>
2022-01-13 18:36:37 +0100

    Refactor aligning header icon

--- src/app/modules/core/components/header/dma-header.component.ts
+++ src/app/modules/core/components/header/dma-header.component.ts
@@ -1,16 +1,18 @@
 import {
-    AfterContentChecked,
+    AfterViewInit,
     ChangeDetectionStrategy,
     ChangeDetectorRef,
     Component,
     ElementRef,
     HostListener,
+    OnDestroy,
     ViewChild,
 } from '@angular/core';
 import { DmaSidebarService } from '@dma-core/services/dma-sidebar/dma-sidebar.service';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { BehaviorSubject, interval, Subject, takeUntil, takeWhile } from 'rxjs';
 
-// Breakpoint for small devives as documented by Bootstrap.
+// Breakpoint for small devices as documented by Bootstrap.
 // Source: https://getbootstrap.com/docs/5.1/layout/breakpoints/
 const BS_MD_BREAKPOINT = 768;
 
@@ -20,35 +22,54 @@ const BS_MD_BREAKPOINT = 768;
     styleUrls: ['./dma-header.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DmaHeaderComponent implements AfterContentChecked {
-    @ViewChild('toggleCollapseNavBtn', { static: true })
-    private toggleCollapseNavBtn: ElementRef;
+export class DmaHeaderComponent implements AfterViewInit, OnDestroy {
+    btnOffset = '';
 
-    constructor(public sidebarService: DmaSidebarService, private changeDetectorRef: ChangeDetectorRef) {}
+    @ViewChild('toggleCollapseNavBtn')
+    private toggleCollapseNavBtn: ElementRef<HTMLButtonElement>;
 
-    ngAfterContentChecked(): void {
-        this.changeDetectorRef.markForCheck();
-    }
+    private destroy = new Subject<void>();
+
+    private btnIconInitialized = new BehaviorSubject<boolean>(false);
+
+    constructor(public sidebarService: DmaSidebarService, private cdRef: ChangeDetectorRef) {}
 
     @HostListener('window:resize')
-    brandOffset(): string {
-        const toggleCollapseNavBtnWidth = parseInt(
-            window.getComputedStyle(this.toggleCollapseNavBtn.nativeElement).width.replace('px', ''),
-            10,
-        );
-        const offset = toggleCollapseNavBtnWidth / 2;
-
-        return this.isCollapsable ? `translateX(${offset}px)` : '';
-    }
-
     get isCollapsable(): boolean {
-        const documentWidth = parseInt(window.getComputedStyle(document.documentElement).width.replace('px', ''), 10);
+        const documentWidth = parseFloat(getComputedStyle(document.documentElement).width.replace('px', ''));
 
         // Look for a document width something slightly smaller than the breakpoint itself.
         return documentWidth < BS_MD_BREAKPOINT - 0.2;
     }
 
+    ngOnDestroy(): void {
+        this.destroy.next();
+        this.destroy.complete();
+    }
+
+    ngAfterViewInit(): void {
+        interval(50)
+            .pipe(
+                takeUntil(this.destroy),
+                takeWhile(() => this.btnIconInitialized.value === false),
+            )
+            .subscribe(() => this.checkBtnIconInitialized());
+    }
+
     get toggleCollapseNavBtnIcon(): IconProp {
         return this.sidebarService.collapsed ? 'bars' : 'times';
+    }
+
+    @HostListener('window:resize')
+    private checkBtnIconInitialized(): void {
+        if (
+            getComputedStyle(this.toggleCollapseNavBtn.nativeElement.firstElementChild.firstElementChild).width ===
+            '0px'
+        )
+            return;
+
+        this.btnOffset = `-${getComputedStyle(this.toggleCollapseNavBtn.nativeElement).width}`;
+        this.btnIconInitialized.next(true);
+        this.cdRef.markForCheck();
     }
 }
