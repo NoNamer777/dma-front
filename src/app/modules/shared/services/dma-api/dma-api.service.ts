b86ec4452e9f765198fa205a049d902e9ea7a1f3
Oscar Wellner <oscar.wellner@gmail.com>
2022-03-22 23:39:52 +0100

    Simplify typing returned server data

--- src/app/modules/shared/services/dma-api/dma-api.service.ts
+++ src/app/modules/shared/services/dma-api/dma-api.service.ts
@@ -1,42 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import ModelsPropertiesMap from '@dma-assets/data/model-properties.json';
-import * as ModelsModule from '@dma-shared';
 import { Entity, Pageable } from '@dma-shared';
+import * as EntityConstructors from '@dma-shared/models/entities';
 import { map, Observable } from 'rxjs';
 
 type MappedModelType = keyof typeof ModelsPropertiesMap;
 
-interface ModelProperty {
-    name: string;
-    type: MappedModelType;
-    collection: boolean;
-}
+type Constructable = new (args: Record<string, unknown>) => unknown;
 
-interface MappedModelProperties {
-    extends: MappedModelType;
-    properties: ModelProperty[];
-}
-
-interface ModelsPropertiesMapType {
-    [model: string]: MappedModelProperties;
-}
-
-interface MappedObject {
-    [key: string]: unknown;
-}
-
-type SimpleValue = boolean | string | number;
-
-type ConstructorFunction = new (...args: SimpleValue[]) => unknown;
-
-type UnknownFunction = (...args: SimpleValue[]) => unknown;
-
-interface MappedObjectCallableFunctions extends MappedObject {
-    [entity: string]: ConstructorFunction | UnknownFunction;
-}
-
-const SIMPLE_VALUE_TYPES = ['string', 'boolean', 'number'];
+const ENTITY_CONSTRUCTORS = EntityConstructors as Record<string, unknown>;
 
 @Injectable({ providedIn: 'root' })
 export class DmaApiService {
@@ -44,118 +17,52 @@ export class DmaApiService {
 
     getResource<T>(url: string, type: MappedModelType): Observable<T> {
         return this.httpClient
-            .get<unknown>(url)
-            .pipe(map((response: unknown) => this.typeResponse<T>(response, type))) as Observable<T>;
+            .get<Record<string, unknown>>(url)
+            .pipe(map((response) => this.typeResponse<T>(response, type))) as Observable<T>;
     }
 
     getPageableResource<T extends Entity>(url: string, type: MappedModelType): Observable<Pageable<T>> {
-        return this.httpClient.get<unknown>(url).pipe(
-            map((response: unknown) => {
-                const pageableSpells = response as Pageable<T>;
-
-                pageableSpells.content = this.typeAllEntities<T>(pageableSpells.content, type);
-
-                return pageableSpells;
-            }),
+        return this.httpClient.get<Pageable<Record<string, unknown>>>(url).pipe(
+            map((response) => ({
+                ...response,
+                content: this.typeAllEntities<T>(response.content, type),
+            })),
         );
     }
 
     postResource<T>(url: string, resource: T, type: MappedModelType): Observable<T> {
         return this.httpClient
-            .post<unknown>(url, resource)
-            .pipe(map((response: unknown) => this.typeResponse<T>(response, type) as T));
+            .post<Record<string, unknown>>(url, resource)
+            .pipe(map((response) => this.typeResponse<T>(response, type) as T));
     }
 
     putResource<T>(url: string, resource: T, type: MappedModelType): Observable<T> {
         return this.httpClient
-            .put<T>(url, resource)
-            .pipe(map((response: unknown) => this.typeResponse<T>(response, type))) as Observable<T>;
+            .put<Record<string, unknown>>(url, resource)
+            .pipe(map((response) => this.typeResponse<T>(response, type))) as Observable<T>;
     }
 
     deleteResource<T>(url: string): Observable<T> {
         return this.httpClient.delete<T>(url);
     }
 
-    private typeResponse<T>(data: unknown, type: MappedModelType): T | T[] {
+    private typeResponse<T>(data: Record<string, unknown>, type: MappedModelType): T | T[] {
         if (data instanceof Array) return this.typeAllEntities<T>(data, type);
 
-        return this.typeEntity<T>(data as MappedObject, type);
+        return this.typeEntity<T>(data, type);
     }
 
-    private typeAllEntities<T>(data: unknown[], type: MappedModelType): T[] {
-        const typed: T[] = [];
-
-        data.forEach((entry) => typed.push(this.typeEntity<T>(entry as MappedObject, type) as T));
-
-        return typed;
+    private typeAllEntities<T>(data: Record<string, unknown>[], type: MappedModelType): T[] {
+        return data.map((entry) => this.typeEntity<T>(entry, type));
     }
 
-    private typeEntity<T>(data: MappedObject | T, type: MappedModelType): T {
-        if (SIMPLE_VALUE_TYPES.includes(typeof data) || SIMPLE_VALUE_TYPES.includes(type)) return data as T;
-
-        const typedEntity = new ((ModelsModule as unknown as MappedObjectCallableFunctions)[
-            type
-        ] as ConstructorFunction)((data as MappedObject)['id'] as string);
-        const properties = this.getEntityProperties(type);
-
-        for (const property of properties) {
-            const value = (data as MappedObject)[property.name];
-
-            // Don't add null values to the typed object result, because they don't add value anyway.
-            if (value != null) {
-                // Handles simple values.
-                if (SIMPLE_VALUE_TYPES.includes(typeof value)) {
-                    (typedEntity as MappedObject)[property.name] = value;
-                }
-
-                // Handles array values.
-                else if (value instanceof Array) {
-                    const addArrayValueFunctionName = this.getAddArrayValueFunctionName(property.name, 1);
-
-                    if (!SIMPLE_VALUE_TYPES.includes(property.type)) {
-                        for (const entry of value) {
-                            (
-                                (typedEntity as MappedObjectCallableFunctions)[
-                                    addArrayValueFunctionName
-                                ] as UnknownFunction
-                            )(this.typeEntity(entry, property.type));
-                        }
-                    } else {
-                        for (const entry of value) {
-                            (
-                                (typedEntity as MappedObjectCallableFunctions)[
-                                    addArrayValueFunctionName
-                                ] as UnknownFunction
-                            )(entry);
-                        }
-                    }
-                }
-
-                // Handles object values.
-                else {
-                    (typedEntity as MappedObject)[property.name] = this.typeEntity(value, property.type);
-                }
-            }
-        }
-        return typedEntity as T;
+    private typeEntity<T>(data: Record<string, unknown>, type: MappedModelType): T {
+        return this.isConstructable(type)
+            ? (new (ENTITY_CONSTRUCTORS[type + 'Model'] as Constructable)(data) as T)
+            : (data as T);
     }
 
-    private getAddArrayValueFunctionName(propertyName: string, end: number): string {
-        return `add${propertyName.charAt(0).toUpperCase()}${propertyName.substring(1, propertyName.length - end)}`;
-    }
-
-    private getEntityProperties(type: MappedModelType): Set<ModelProperty> {
-        const mappedEntity = (ModelsPropertiesMap as ModelsPropertiesMapType)[type];
-        const properties = new Set(mappedEntity.properties);
-
-        if (mappedEntity.extends) {
-            this.getEntityProperties(mappedEntity.extends).forEach((property) => properties.add(property));
-        }
-
-        properties.forEach((property) => {
-            if (property.name === 'id') properties.delete(property);
-        });
-
-        return properties;
+    private isConstructable(type: MappedModelType): boolean {
+        return ENTITY_CONSTRUCTORS[type + 'Model'] !== undefined;
     }
 }
